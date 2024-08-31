7485c00cda9a09cbfd7cd179838927b6c59e152f
Gilbert Kemell <kemegil515@gmail.com>
2024-08-31 15:07:21 -0300

    :sparkles: feat: Configuring dependency injection

--- XerifeTv.CMS/Program.cs
+++ XerifeTv.CMS/Program.cs
@@ -1,17 +1,20 @@
+using XerifeTv.CMS;
+using XerifeTv.CMS.MongoDB;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.Configure<DBSettings>(
+  builder.Configuration.GetSection("MongoDBConfig"));
+
+builder.Services.AddConfiguration();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-  app.UseExceptionHandler("/Home/Error");
-  // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-  app.UseHsts();
-}
+if (!app.Environment.IsDevelopment()) app.UseHsts();
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
