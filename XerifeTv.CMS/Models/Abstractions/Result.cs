5da0d05e93847cb3ceab0aa1ece714e640c7643a
Gilbert Kemell <kemegil515@gmail.com>
2024-08-31 15:05:15 -0300

    :sparkles: feat: Implementation of the Result pattern

---
+++ XerifeTv.CMS/Models/Abstractions/Result.cs
@@ -0,0 +1,27 @@
+﻿namespace XerifeTv.CMS.Models.Abstractions;
+
+public sealed record Error(string Code, string? Description = null)
+{
+  public static readonly Error None = new(string.Empty);
+}
+
+public class Result<T>
+{
+  private Result(bool isSuccess, Error error, T? data = default)
+  {
+    if (!isSuccess && error == Error.None || isSuccess && error != Error.None)
+      throw new ArgumentException("Invalid error", nameof(error));
+
+    IsSuccess = isSuccess;
+    Error = error;
+    Data = data;
+  }
+
+  public bool IsSuccess { get; }
+  public bool IsFailure => !IsSuccess;
+  public Error Error { get; }
+  public T? Data { get; } = default;
+
+  public static Result<T> Success(T data) => new(true, Error.None, data);
+  public static Result<T> Failure(Error error) => new(false, error);
+}
