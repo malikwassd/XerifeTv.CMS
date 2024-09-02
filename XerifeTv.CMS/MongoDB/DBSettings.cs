cd2485b1d3c6e8359565eeb4936b31358a69d15c
gilbertsilvaa <kemegil515@gmail.com>
2024-09-01 23:25:17 -0300

    :sparkles: feat: using class because it is necessary

--- XerifeTv.CMS/MongoDB/DBSettings.cs
+++ XerifeTv.CMS/MongoDB/DBSettings.cs
@@ -1,6 +1,8 @@
 ﻿namespace XerifeTv.CMS.MongoDB;
 
-public sealed record DBSettings(
-   string ConnectionString,
-   string DatabaseName);
+public class DBSettings
+{
+  public string ConnectionString { get; set; } = string.Empty;
+  public string DatabaseName { get; set; } = string.Empty;
+}
 
