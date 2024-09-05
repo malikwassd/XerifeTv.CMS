8c56911ca67313a977068a60bd52c3f88edc4cb0
Gilbert Kemell <kemegil515@gmail.com>
2024-09-05 20:08:22 -0300

    :sparkles: feat: paged list model created

---
+++ XerifeTv.CMS/Models/Abstractions/PagedList.cs
@@ -0,0 +1,15 @@
+﻿namespace XerifeTv.CMS.Models.Abstractions;
+
+public sealed class PagedList<T>(
+  int pageSize, 
+  int currentPage,
+  int totalPageCount, 
+  IEnumerable<T> items) where T : class
+{
+  public int PageSize { get; private set; } = pageSize;
+  public int CurrentPage { get; private set; } = currentPage;
+  public int TotalPageCount { get; private set; } = totalPageCount;
+  public IEnumerable<T> Items { get; private set; } = items;
+  public bool HasPrevious => (CurrentPage > 1);
+  public bool HasNext => (CurrentPage < TotalPageCount);
+}
