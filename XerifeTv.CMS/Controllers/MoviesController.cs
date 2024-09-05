78772fc5aacbb49f6ba1b9512bf259fc366a8300
Gilbert Kemell <kemegil515@gmail.com>
2024-09-05 19:27:10 -0300

    :recycle: refactor: placing the movie filter in a separate method in the controller that is triggered via http post.

--- XerifeTv.CMS/Controllers/MoviesController.cs
+++ XerifeTv.CMS/Controllers/MoviesController.cs
@@ -1,8 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using XerifeTv.CMS.Models.Movie.Enums;
-using XerifeTv.CMS.Models.Abstractions;
 using XerifeTv.CMS.Models.Movie.Dtos.Request;
-using XerifeTv.CMS.Models.Movie.Dtos.Response;
 using XerifeTv.CMS.Models.Movie.Interfaces;
 
 namespace XerifeTv.CMS.Controllers;
@@ -11,53 +9,22 @@ public class MoviesController(IMovieService _service) : Controller
 {
   public async Task<IActionResult> Index()
   {
-    Result<IEnumerable<GetMoviesResponseDto>> result;
+    var result = await _service.Get();
 
-    if (Request.QueryString.HasValue)
-    {
-      var search = Request.QueryString.Value
-        ?.Split('?')[1]
-        ?.Trim()
-        ?.Replace("%20", " ");
+    ViewData["data"] = result.IsSuccess ? result.Data : [];
 
-      var searchParams = search?.Split("=");
+    return View();
+  }
 
-      string? filterSearch;
-      string? valueSearch;
+  [HttpPost]
+  public async Task<IActionResult> Index(ESearchFilter filter, string search)
+  {
+    var result = await _service.GetByFilter(filter, search);
 
-      if (searchParams?.Length < 2)
-      {
-        filterSearch = "title";
-        valueSearch = searchParams?[0];
-      }
-      else
-      {
-        filterSearch = searchParams?[0];
-        valueSearch = searchParams?[1];
-      }
+    ViewBag.search = search;
+    ViewBag.filter = filter.ToString();
 
-      ViewData["search"] = valueSearch;
-      ViewData["filter"] = filterSearch;
-
-      var filter = ESearchFilter.TITLE;
-
-      if (filterSearch == "category")
-      {
-        filter = ESearchFilter.CATEGORY;
-      }
-      else if (filterSearch == "releaseYear")
-      {
-        filter = ESearchFilter.RELEASE_YEAR;
-      }
-
-      result = await _service.GetByFilter(filter, valueSearch ?? "");
-    }
-    else
-    {
-      result = await _service.Get();
-    }
-
-    if (result.IsSuccess) ViewData["data"] = result.Data;
+    ViewData["data"] = result.IsSuccess ? result.Data : [];
 
     return View();
   }
