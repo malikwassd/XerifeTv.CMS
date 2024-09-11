2e757ca045bb674a6f0ea84ed75f93bc3c58a187
Gilbert Kemell <kemegil515@gmail.com>
2024-09-11 14:49:44 -0300

    :sparkles: feat: movie details modal implemented

--- XerifeTv.CMS/Models/Movie/Dtos/Response/GetMoviesResponseDto.cs
+++ XerifeTv.CMS/Models/Movie/Dtos/Response/GetMoviesResponseDto.cs
@@ -4,9 +4,13 @@ public class GetMoviesResponseDto
 {
   public string Id { get; private set; } = string.Empty;
   public string Title { get; private set; } = string.Empty;
+  public string Synopsis { get; private set; } = string.Empty;
   public string Category { get; private set; } = string.Empty;
   public string PosterUrl { get; private set; } = string.Empty;
+  public string BannerUrl { get; private set; } = string.Empty;
   public int ReleaseYear { get; private set; }
+  public int ParentalRating { get; private set; }
+  public float Review { get; private set; }
   public DateTime RegistrationDate { get; private set; }  
 
   public static GetMoviesResponseDto FromEntity(MovieEntity entity)
@@ -15,9 +19,13 @@ public class GetMoviesResponseDto
     {
       Id = entity.Id,
       Title = entity.Title,
+      Synopsis = entity.Synopsis,
       Category = entity.Category,
       PosterUrl = entity.PosterUrl,
+      BannerUrl = entity.BannerUrl,
       ReleaseYear = entity.ReleaseYear,
+      ParentalRating = entity.ParentalRating,
+      Review = entity.Review,
       RegistrationDate = entity.CreateAt
     }; 
   }
