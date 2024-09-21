cf28280f0ea05d19154cf1063f26a31f88165f28
Gilbert Kemell <kemegil009@gmail.com>
2024-09-21 12:53:15 -0300

    :sparkles: feat: channels controller and view created

---
+++ XerifeTv.CMS/Controllers/ChannelsController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace XerifeTv.CMS.Controllers;
+
+public class ChannelsController : Controller
+{
+  public ActionResult Index()
+  {
+    return View();
+  }
+}
+
