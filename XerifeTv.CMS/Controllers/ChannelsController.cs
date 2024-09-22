8b651329ad52e3ebe4a36d153c359556aa3e568b
gilbertsilvaa <kemegil515@gmail.com>
2024-09-22 20:45:02 -0300

    :sparkles: feat: implemented channel views (index and form)

--- XerifeTv.CMS/Controllers/ChannelsController.cs
+++ XerifeTv.CMS/Controllers/ChannelsController.cs
@@ -1,12 +1,76 @@
 using Microsoft.AspNetCore.Mvc;
+using XerifeTv.CMS.Models.Abstractions;
+using XerifeTv.CMS.Models.Channel.Dtos.Request;
+using XerifeTv.CMS.Models.Channel.Dtos.Response;
+using XerifeTv.CMS.Models.Channel.Enums;
+using XerifeTv.CMS.Models.Channel.Interfaces;
 
 namespace XerifeTv.CMS.Controllers;
 
-public class ChannelsController : Controller
+public class ChannelsController(IChannelService _service) : Controller
 {
-  public ActionResult Index()
+  private const int limitResultsPage = 15;
+
+  public async Task<IActionResult> Index(int? currentPage, EChannelSearchFilter? filter, string? search)
   {
+    Result<PagedList<GetChannelResponseDto>>? result;
+
+    if (filter is EChannelSearchFilter && !string.IsNullOrEmpty(search))
+    {
+      result = await _service.GetByFilter(
+        new GetChannelsByFilterRequestDto(filter, search, limitResultsPage, currentPage));
+
+      ViewBag.Search = search;
+      ViewBag.Filter = filter.ToString()?.ToLower();
+    }
+    else
+    {
+      result = await _service.Get(currentPage ?? 1, limitResultsPage);
+    }
+
+    if (result.IsSuccess)
+    {
+      ViewBag.CurrentPage = result.Data?.CurrentPage;
+      ViewBag.TotalPages = result.Data?.TotalPageCount ?? 1;
+      ViewBag.HasNextPage = result.Data?.HasNext;
+      ViewBag.HasPrevPage = result.Data?.HasPrevious;
+
+      return View(result.Data?.Items);
+    }
+
+    return View(Enumerable.Empty<GetChannelResponseDto>());
+  }
+
+  public async Task<IActionResult> Form(string? id)
+  {
+    if (id is not null)
+    {
+      var response = await _service.Get(id);
+      if (response.IsSuccess) return View(response.Data);
+    }
+
     return View();
+  }
+
+  public async Task<IActionResult> Create(CreateChannelRequestDto dto)
+  {
+    await _service.Create(dto);
+
+    return RedirectToAction("Index");
+  }
+
+  public async Task<IActionResult> Update(UpdateChannelRequestDto dto)
+  {
+    await _service.Update(dto);
+
+    return RedirectToAction("Index");
+  }
+
+  public async Task<IActionResult> Delete(string? id)
+  {
+    if (id is not null) await _service.Delete(id);
+
+    return RedirectToAction("Index");
   }
 }
 
