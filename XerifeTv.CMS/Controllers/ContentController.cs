fec62760ecf6c37b590f35c66e69c315d0588f18
gilbertsilvaa <kemegil515@gmail.com>
2024-10-01 21:11:29 -0300

    :recycle: refactor: passing content controller to api controller

--- XerifeTv.CMS/Controllers/ContentController.cs
+++ XerifeTv.CMS/Controllers/ContentController.cs
@@ -3,59 +3,55 @@ using XerifeTv.CMS.Models.Content.Interfaces;
 
 namespace XerifeTv.CMS.Controllers;
 
-public class ContentController(IContentService _service) : Controller
+[Route("Api/Content")]
+[ApiController]
+public class ContentController(IContentService _service) : ControllerBase
 {
-  public async Task<JsonResult> Movies(int? limit)
+  [HttpGet]
+  [Route("Movies")]
+  public async Task<IActionResult> Movies(int? limit)
   {
     var response = await _service.GetMoviesGroupByCategory(limit);
-
-    if (response.IsSuccess) return Json(response.Data);
-
-    return Json(Enumerable.Empty<string>());
+    return Ok(response.IsSuccess ? response.Data : Enumerable.Empty<string>());
   }
 
-  public async Task<JsonResult> MoviesCategory(string category, int? limit)
+  [HttpGet]
+  [Route("Movies/Category")]
+  public async Task<IActionResult> MoviesCategory(string category, int? limit)
   {
     var response = await _service.GetMoviesByCategory(category, limit);
-
-    if (response.IsSuccess) return Json(response.Data);
-
-    return Json(Enumerable.Empty<string>());
+    return Ok(response.IsSuccess ? response.Data : Enumerable.Empty<string>());
   }
 
-  public async Task<JsonResult> Series(int? limit)
+  [HttpGet]
+  [Route("Series")]
+  public async Task<IActionResult> Series(int? limit)
   {
     var response = await _service.GetSeriesGroupByCategory(limit);
-
-    if (response.IsSuccess) return Json(response.Data);
-
-    return Json(Enumerable.Empty<string>());
+    return Ok(response.IsSuccess ? response.Data : Enumerable.Empty<string>());
   }
 
-  public async Task<JsonResult> SeriesCategory(string category, int? limit)
+  [HttpGet]
+  [Route("Series/Category")]
+  public async Task<IActionResult> SeriesCategory(string category, int? limit)
   {
     var response = await _service.GetSeriesByCategory(category, limit);
-
-    if (response.IsSuccess) return Json(response.Data);
-
-    return Json(Enumerable.Empty<string>());
+    return Ok(response.IsSuccess ? response.Data : Enumerable.Empty<string>());
   }
 
-  public async Task<JsonResult> SeriesEpisodes(string serieId, int season)
+  [HttpGet]
+  [Route("Series/Episodes")]
+  public async Task<IActionResult> SeriesEpisodes(string serieId, int season)
   {
     var response = await _service.GetEpisodesSeriesBySeason(serieId, season);
-
-    if (response.IsSuccess) return Json(response.Data);
-
-    return Json(Enumerable.Empty<string>());
+    return Ok(response.IsSuccess ? response.Data : Enumerable.Empty<string>());
   }
 
-  public async Task<JsonResult> Channels(int? limit)
+  [HttpGet]
+  [Route("Channels")]
+  public async Task<IActionResult> Channels(int? limit)
   {
     var response = await _service.GetChannelsGroupByCategory(limit);
-
-    if (response.IsSuccess) return Json(response.Data);
-
-    return Json(Enumerable.Empty<string>());
+    return Ok(response.IsSuccess ? response.Data : Enumerable.Empty<string>());
   }
 }
