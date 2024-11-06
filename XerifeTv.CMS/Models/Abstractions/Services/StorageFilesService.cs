74a460cd0abcb10c1b1b0f56276aa232ed933e09
GilbertSilvaa <kemegil515@gmail.com>
2024-11-06 12:39:21 -0300

    :sparkles: feat: implementing file storage service with supabase

--- XerifeTv.CMS/Models/Abstractions/Services/StorageFilesService.cs
+++ XerifeTv.CMS/Models/Abstractions/Services/StorageFilesService.cs
@@ -1,17 +1,39 @@
-﻿namespace XerifeTv.CMS.Models.Abstractions.Services;
+﻿using Supabase;
+
+namespace XerifeTv.CMS.Models.Abstractions.Services;
 
 public class StorageFilesService : IStorageFilesService
 {
-  private readonly IEnumerable<string> AcceptedExtensions = new[] { ".vtt" };
+  private readonly string[] _acceptedExtensions = new[] { ".vtt" };
+  private readonly Client? _client = default;
+
+  public StorageFilesService(IConfiguration _configuration)
+  {
+    _client = new Supabase.Client(
+      _configuration["Supabase:Url"], 
+      _configuration["Supabase:Key"]);
+  }
   
   public async Task<Result<string>> UploadFileAsync(Stream fileStream, string fileName)
   {
     var fileExtension = Path.GetExtension(fileName);
 
-    if (!AcceptedExtensions.Contains(fileExtension))
-      return Result<string>.Failure(
-        new Error("400", $"file extension {fileExtension} is not accepted."));
+    if (!_acceptedExtensions.Contains(fileExtension))
+      return Result<string>.Failure(new Error("400", $"file extension {fileExtension} is not accepted."));
+
+    await _client.InitializeAsync();
     
-    return Result<string>.Success(fileName);
+    using var ms = new MemoryStream(); 
+    await fileStream.CopyToAsync(ms);
+    
+    var response = await _client.Storage
+      .From("subtitles")
+      .Upload(ms.ToArray(), fileName, new() {Upsert = true});
+
+    if (response == null) 
+      return Result<string>.Failure(new Error("400", "error uploading file"));
+
+    var urlFile = $"{_client.Auth.Options.Url.Split("/auth")[0]}/storage/v1/object/public/{response}";
+    return Result<string>.Success(urlFile);
   }
 }
