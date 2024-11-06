2ad0f2d8ace5a88d721d67980e55cc7de2891fa0
gilbertsilvaa <kemegil515@gmail.com>
2024-11-05 23:23:24 -0300

    :sparkles: feat: storage file service created

--- XerifeTv.CMS/ConfigureServices.cs
+++ XerifeTv.CMS/ConfigureServices.cs
@@ -51,6 +51,7 @@ public static class ConfigureServices
     services.AddScoped<IUserService, UserService>();
     services.AddScoped<ITokenService, TokenService>();
     services.AddScoped<ICacheService, CacheService>();
+    services.AddScoped<IStorageFilesService, StorageFilesService>();
     return services;
   }
 
---
+++ XerifeTv.CMS/Models/Abstractions/Services/StorageFilesService.cs
@@ -0,0 +1,17 @@
+﻿namespace XerifeTv.CMS.Models.Abstractions.Services;
+
+public class StorageFilesService : IStorageFilesService
+{
+  private readonly IEnumerable<string> AcceptedExtensions = new[] { ".vtt" };
+  
+  public async Task<Result<string>> UploadFileAsync(Stream fileStream, string fileName)
+  {
+    var fileExtension = Path.GetExtension(fileName);
+
+    if (!AcceptedExtensions.Contains(fileExtension))
+      return Result<string>.Failure(
+        new Error("400", $"file extension {fileExtension} is not accepted."));
+    
+    return Result<string>.Success(fileName);
+  }
+}
